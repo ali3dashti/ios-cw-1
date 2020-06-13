3c3f497b7a996a21d70f1bbaa1cebbfc74f38715
ali3dashti <aadashti73@gmail.com>
2020-06-13 20:03:04 +0300

    تم الحل

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,10 +8,32 @@ var hobby1 = "football"
     
 var hobby2 = "playstation"
 
-var age = 17
+var age = 19
 
 var height = 173
 
+print("my name is \(name), and my hobby1 is \(hobby1),and my hobby2 is \(hobby2),amd my age is \(age),and my height is \(height)")
 
+let friendname = "ali"
 
+var friendHobby1 = "football"
 
+var friendHobby2 = "volleyball"
+
+var friendAge = 17
+
+var friendHeight = 173
+
+var age_difference = (age - friendAge)
+
+var height_difference = (height - friendHeight)
+
+name == friendname
+ 
+hobby1 == friendHobby1
+
+hobby2 == friendHobby2
+
+height == friendHeight
+
+age == friendAge
