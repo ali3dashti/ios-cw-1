e16bcd9995af69b5ebdd7512ed8ffff2359ce913
ali3dashti <aadashti73@gmail.com>
2020-06-13 19:18:45 +0300

    تم

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "CW-1-علي عدنان دشتي -429 "
+
+var hobby1 = "football"
+    
+var hobby2 = "playstation"
+
+var age = 17
+
+var height = 173
+
+
+
+
